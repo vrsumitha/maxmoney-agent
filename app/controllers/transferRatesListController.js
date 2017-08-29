bfa248a7d2d120035f5718a25bdc0756c29fd257
Sumitha Vasanthan <sumitha.v.r@gmail.com>
2017-08-29 13:30:28 +0530

    Merge branch 'master' into issue-42
    # Conflicts:
    #	app/appControllers.js
    
    fix for issue-42  - rates which has country keys are all displayed

--- app/controllers/transferRatesListController.js
+++ app/controllers/transferRatesListController.js
@@ -7,11 +7,26 @@ function transferRatesListController($log, $rootScope, $scope, wydNotifyService,
     var vm = this, uiState = {isReady: false, isBlocked: false, isValid: false};
     vm.currencies = [];
 
+    function processRates(rates) {
+      //  console.log(rates);
+        var keys = _.keys(rates);
+        _.forEach(keys, function(key) {
+            //console.log(key + ' = ' + rates[key]);
+            if(key.endsWith('_country') && rates[key]) {
+                //console.log(key + ' = ' + rates[key]);
+                var cur = {};
+                cur.code = key.substr(0, 3);
+                cur.country = rates[cur.code + '_country'];
+                cur.transferRate = rates[cur.code + '_tt'];
+                vm.currencies.push(cur);
+            }
+        });
+    }
+
     function loadRates() {
         $log.debug('loadRates started...');
 
-        _.forEach(vm.currencies, function(item) {
-            var path = sessionService.getApiBasePath() + item.apiSuffix;
+            var path = sessionService.getApiBasePath() + '/boards/moos';
             var req = {
                 method: 'GET',
                 url: path,
@@ -19,55 +34,19 @@ function transferRatesListController($log, $rootScope, $scope, wydNotifyService,
             };
            // $log.info(req);
             $http(req).then(function (res) {
-                $log.debug(res);
-                $log.debug('loadRate ' + item.code + ' finished with success.');
-                item.transferRate = res.data[item.code + '_tt'];
+               // $log.debug(res);
+               // $log.debug('loadRates finished with success.');
+                processRates(res.data);
             }, function (res) {
                 $log.error(res);
-                $log.debug('loadRate ' + item.code + ' finished with failure.');
+                $log.debug('loadRates finished with failure.');
             });
-        });
         $log.debug('loadRates finished...');
     }
 
     function init() {
         $log.info('init started...');
 
-        var cur = {};
-        cur.code = 'php';
-        cur.country = 'Philipines';
-        cur.apiSuffix = '/boards/moos/rates/' + cur.code;
-        cur.transferRate = 0;
-        vm.currencies.push(cur);
-
-        cur = {};
-        cur.code = 'inr';
-        cur.country = 'India';
-        cur.apiSuffix = '/boards/moos/rates/' + cur.code;
-        cur.transferRate = 0;
-        vm.currencies.push(cur);
-
-        cur = {};
-        cur.code = 'pkr';
-        cur.country = 'pakistan';
-        cur.apiSuffix = '/boards/moos/rates/' + cur.code;
-        cur.transferRate = 0;
-        vm.currencies.push(cur);
-
-        cur = {};
-        cur.code = 'idr';
-        cur.country = 'Indonesia';
-        cur.apiSuffix = '/boards/moos/rates/' + cur.code;
-        cur.transferRate = 0;
-        vm.currencies.push(cur);
-
-        cur = {};
-        cur.code = 'npr';
-        cur.country = 'Nepals';
-        cur.apiSuffix = '/boards/moos/rates/' + cur.code;
-        cur.transferRate = 0;
-        vm.currencies.push(cur);
-        console.log(vm.currencies);
         loadRates();
 
         $log.info('init finished...');
