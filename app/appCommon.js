596a4d89edff641debceae13300ba82307e9ce63
Eialarasu <vteial@gmail.com>
2017-08-30 14:11:45 +0530

    fix for issue-65 - hiding action buttons when user is active

--- app/appCommon.js
+++ app/appCommon.js
@@ -103,9 +103,3 @@ appDirectives.directive('fileModel', ['$parse', function ($parse) {
         }
     };
 }]);
-
-// console.log('Frontend Hostname : ' + window.location.hostname);
-// if (window.location.hostname == 'www.maxmoney.com') {
-//     apiBasePath = 'https://api.maxmoney.com/v1';
-// }
-// console.log('Backend URL : ' + apiBasePath);
